194bf34f635a82d3acd5a88c833f9ca4507f86f6
Andrea Falco <fox91fox@gmail.com>
2022-01-03 16:01:15 +0100

    Added submodules (#292)

--- generator/doc/update.sh
+++ generator/doc/update.sh
@@ -1,11 +1,4 @@
-if [ ! -d "doc-en" ]; then
-    mkdir doc-en
-    cd doc-en
-    git clone https://github.com/salathe/phpdoc-base doc-base
-    git clone https://github.com/php/doc-en en
-  else
-    cd doc-en/en
-    git pull
-    cd ../doc-base
-    git pull
-  fi
+#!/usr/bin/env sh
+
+git submodule init
+git submodule update --remote --merge

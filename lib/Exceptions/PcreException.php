16883cda9b66b76c7b146b968347a509c470c69a
Julio Foulquie <julio@thenextweb.com>
2022-03-30 15:48:01 -0300

    Include the constant name on PcreException message as preffix and fix test

--- lib/Exceptions/PcreException.php
+++ lib/Exceptions/PcreException.php
@@ -7,6 +7,15 @@ class PcreException extends \Exception implements SafeExceptionInterface
 {
     public static function createFromPhpError(): self
     {
-        return new self(\preg_last_error_msg(), \preg_last_error());
+        $errorConstantMap = [
+            PREG_INTERNAL_ERROR => 'PREG_INTERNAL_ERROR',
+            PREG_BACKTRACK_LIMIT_ERROR => 'PREG_BACKTRACK_LIMIT_ERROR',
+            PREG_RECURSION_LIMIT_ERROR => 'PREG_RECURSION_LIMIT_ERROR',
+            PREG_BAD_UTF8_ERROR => 'PREG_BAD_UTF8_ERROR',
+            PREG_BAD_UTF8_OFFSET_ERROR => 'PREG_BAD_UTF8_OFFSET_ERROR',
+            PREG_JIT_STACKLIMIT_ERROR => 'PREG_JIT_STACKLIMIT_ERROR',
+        ];
+
+        return new self(($errorConstantMap[\preg_last_error()] ?? 'Unknown Error') . ': ' . \preg_last_error_msg(), \preg_last_error());
     }
 }

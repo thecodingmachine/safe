54df8b30bb9f2730af59e3d20b639f866bf73419
Cristi Radu <indy2kro@gmail.com>
2024-07-22 20:23:59 +0300

    Update special_cases.php

--- lib/special_cases.php
+++ lib/special_cases.php
@@ -8,8 +8,6 @@
 
 namespace Safe;
 
-use Safe\Exceptions\FilesystemException;
-
 use const PREG_NO_ERROR;
 
 use Safe\Exceptions\MiscException;
@@ -21,6 +19,8 @@ use Safe\Exceptions\JsonException;
 use Safe\Exceptions\OpensslException;
 use Safe\Exceptions\PcreException;
 use Safe\Exceptions\SimplexmlException;
+
+use Safe\Exceptions\FilesystemException;
 
 /**
  * Wrapper for json_decode that throws when an error occurs.

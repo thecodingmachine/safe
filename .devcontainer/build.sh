962eda41daa94e67aa09a23e4f02b69329ae9bd0
Shish <shish@shishnet.org>
2025-01-19 23:52:50 +0000

    [docker] exit early if build fails
    If the build fails, don't then attempt to run the container-which-wasn't-built

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+set -eu
+
+# A little helper script to rebuild the generated files using a known-good
+# environment, so that we should all end up with the same results, and also
+# no need to install any dependencies on the host machine.
+
 cd $(dirname $0)/../
 docker run --rm -v ${PWD}:/app $(docker build -q -f .devcontainer/Dockerfile .)
--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+set -eu
+
+# A little helper script to run a shell in a known-good environment, so that
+# developers can easily debug issues with the build process.
+
 cd $(dirname $0)/../
 docker run --rm -ti -v ${PWD}:/app $(docker build -q . -f .devcontainer/Dockerfile) /bin/bash

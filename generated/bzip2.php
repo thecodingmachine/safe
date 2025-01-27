2334b87c6bc00b61269b8476320344ebd2cf5eb9
Shish <shish@shishnet.org>
2025-01-27 15:22:09 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which were generated in `8.1/apache.php` but are no longer needed and thus not-generated in `8.2/apache.php` will end up in `8.2/deprecated.php`. Perhaps it'd be better to put them at the tail of `8.2/apache.php`? PRs welcome)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    This isn't even remotely tested, just a proof of concept to show what this kind of approach might look like

--- generated/bzip2.php
+++ generated/bzip2.php
@@ -1,97 +1,17 @@
 <?php
 
-namespace Safe;
-
-use Safe\Exceptions\Bzip2Exception;
-
-/**
- * Closes the given bzip2 file pointer.
- *
- * @param resource $bz The file pointer. It must be valid and must point to a file
- * successfully opened by bzopen.
- * @throws Bzip2Exception
- *
- */
-function bzclose($bz): void
-{
-    error_clear_last();
-    $safeResult = \bzclose($bz);
-    if ($safeResult === false) {
-        throw Bzip2Exception::createFromPhpError();
-    }
+if (strpos(PHP_VERSION, "8.1.") === 0) {
+    require_once __DIR__ . '/8.1/bzip2.php';
 }
-
-
-/**
- * This function is supposed to force a write of all buffered bzip2 data for the file pointer
- * bz,
- * but is implemented as null function in libbz2, and as such does nothing.
- *
- * @param resource $bz The file pointer. It must be valid and must point to a file
- * successfully opened by bzopen.
- * @throws Bzip2Exception
- *
- */
-function bzflush($bz): void
-{
-    error_clear_last();
-    $safeResult = \bzflush($bz);
-    if ($safeResult === false) {
-        throw Bzip2Exception::createFromPhpError();
-    }
+if (strpos(PHP_VERSION, "8.2.") === 0) {
+    require_once __DIR__ . '/8.2/bzip2.php';
 }
-
-
-/**
- * bzread reads from the given bzip2 file pointer.
- *
- * Reading stops when length (uncompressed) bytes have
- * been read or EOF is reached, whichever comes first.
- *
- * @param resource $bz The file pointer. It must be valid and must point to a file
- * successfully opened by bzopen.
- * @param int $length If not specified, bzread will read 1024
- * (uncompressed) bytes at a time. A maximum of 8192
- * uncompressed bytes will be read at a time.
- * @return string Returns the uncompressed data.
- * @throws Bzip2Exception
- *
- */
-function bzread($bz, int $length = 1024): string
-{
-    error_clear_last();
-    $safeResult = \bzread($bz, $length);
-    if ($safeResult === false) {
-        throw Bzip2Exception::createFromPhpError();
-    }
-    return $safeResult;
+if (strpos(PHP_VERSION, "8.3.") === 0) {
+    require_once __DIR__ . '/8.3/bzip2.php';
 }
-
-
-/**
- * bzwrite writes a string into the given bzip2 file
- * stream.
- *
- * @param resource $bz The file pointer. It must be valid and must point to a file
- * successfully opened by bzopen.
- * @param string $data The written data.
- * @param int $length If supplied, writing will stop after length
- * (uncompressed) bytes have been written or the end of
- * data is reached, whichever comes first.
- * @return int Returns the number of bytes written.
- * @throws Bzip2Exception
- *
- */
-function bzwrite($bz, string $data, ?int $length = null): int
-{
-    error_clear_last();
-    if ($length !== null) {
-        $safeResult = \bzwrite($bz, $data, $length);
-    } else {
-        $safeResult = \bzwrite($bz, $data);
-    }
-    if ($safeResult === false) {
-        throw Bzip2Exception::createFromPhpError();
-    }
-    return $safeResult;
+if (strpos(PHP_VERSION, "8.4.") === 0) {
+    require_once __DIR__ . '/8.4/bzip2.php';
+}
+if (strpos(PHP_VERSION, "8.5.") === 0) {
+    require_once __DIR__ . '/8.5/bzip2.php';
 }

03db313f972e466f73463b605f8e52aed4185268
Shish <shish@shishnet.org>
2025-01-27 13:10:17 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which were generated in `8.1/apache.php` but are no longer needed and thus not-generated in `8.2/apache.php` will end up in `8.2/deprecated.php`. Perhaps it'd be better to put them at the tail of `8.2/apache.php`? PRs welcome)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    This isn't even remotely tested, just a proof of concept to show what this kind of approach might look like

--- generated/xmlrpc.php
+++ generated/xmlrpc.php
@@ -1,22 +1,17 @@
 <?php
 
-namespace Safe;
-
-use Safe\Exceptions\XmlrpcException;
-
-/**
- * Sets xmlrpc type, base64 or datetime, for a PHP string value.
- *
- * @param string|\DateTime $value Value to set the type
- * @param string $type 'base64' or 'datetime'
- * @throws XmlrpcException
- *
- */
-function xmlrpc_set_type(&$value, string $type): void
-{
-    error_clear_last();
-    $safeResult = \xmlrpc_set_type($value, $type);
-    if ($safeResult === false) {
-        throw XmlrpcException::createFromPhpError();
-    }
+if (strpos(PHP_VERSION, "8.1.") === 0) {
+    require_once __DIR__ . '/8.1/xmlrpc.php';
+}
+if (strpos(PHP_VERSION, "8.2.") === 0) {
+    require_once __DIR__ . '/8.2/xmlrpc.php';
+}
+if (strpos(PHP_VERSION, "8.3.") === 0) {
+    require_once __DIR__ . '/8.3/xmlrpc.php';
+}
+if (strpos(PHP_VERSION, "8.4.") === 0) {
+    require_once __DIR__ . '/8.4/xmlrpc.php';
+}
+if (strpos(PHP_VERSION, "8.5.") === 0) {
+    require_once __DIR__ . '/8.5/xmlrpc.php';
 }

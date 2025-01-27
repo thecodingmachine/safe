0d40a34dc149936cefd3b91ce4264c31032f8036
Shish <shish@shishnet.org>
2025-01-27 12:00:26 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which were generated in `8.1/apache.php` but are no longer needed and thus not-generated in `8.2/apache.php` will end up in `8.2/deprecated.php`. Perhaps it'd be better to put them at the tail of `8.2/apache.php`? PRs welcome)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    This isn't even remotely tested, just a proof of concept to show what this kind of approach might look like

--- generator/src/GenerateCommand.php
+++ generator/src/GenerateCommand.php
@@ -23,39 +23,60 @@ class GenerateCommand extends Command
     {
 
         $this->rmGenerated();
+
         // Let's build the DTD necessary to load the XML files.
         DocPage::buildEntities();
-        $scanner = new Scanner(__DIR__ . '/../doc/doc-en/en/reference/');
 
-        $paths = $scanner->getFunctionsPaths();
-
-        $res = $scanner->getMethods($paths);
-        $functions = $res->methods;
-        $overloadedFunctions = $res->overloadedFunctions;
-
-        $output->writeln('These functions have been ignored and must be dealt with manually: '.\implode(', ', $overloadedFunctions));
-
-        $fileCreator = new FileCreator();
-        $fileCreator->generatePhpFile($functions, __DIR__ . '/../../generated/');
-        $fileCreator->generateFunctionsList($functions, __DIR__ . '/../../generated/functionsList.php');
-        $fileCreator->generateRectorFile($functions, __DIR__ . '/../../rector-migrate.php');
-
-
+        $versions = [
+            "8.1" => "9097ea48f608dbbbf795235a31af82b85bd94430",
+            "8.2" => "8f4e8cf3de08208e71eb0117f1c970c27e9120c9",
+            "8.3" => "7453a50321f0834421cebea8edade14deef5466b",
+            "8.4" => "d553fa36940639b0889ec4358fa3bbb92f123b69",
+            "8.5" => "master",
+        ];
         $modules = [];
-        foreach ($functions as $function) {
-            $moduleName = $function->getModuleName();
-            $modules[$moduleName] = $moduleName;
+        $allFunctions = [];
+
+        foreach ($versions as $version => $commit) {
+            $refDir = __DIR__ . '/../doc/doc-en/en/reference/';
+            (new Process(['git', "checkout", $commit], $refDir))->run();
+
+            $scanner = new Scanner($refDir);
+            $res = $scanner->getMethods($scanner->getFunctionsPaths());
+            $output->writeln('These functions have been ignored and must be dealt with manually: '.\implode(', ', $res->overloadedFunctions));
+            $allFunctions = \array_merge($allFunctions, $res->methods);
+            foreach ($res->methods as $function) {
+                $modules[$function->getModuleName()] = true;
+            }
+
+            // If a method needed a safe wrapper in eg 8.1, but 8.2 is safe natively,
+            // we should add a no-op wrapper in 8.2 to keep backwards compatibility.
+            $missingMethods = [];
+            foreach ($allFunctions as $function) {
+                $missingMethods[] = $function;
+                $output->writeln('Method no longer needs safe wrapper in ' . $version . ': ' . $function->getFunctionName());
+            }
+
+            $genDir = __DIR__ . "/../../generated/$version";
+            $fileCreator = new FileCreator();
+            $fileCreator->generatePhpFile($res->methods, "$genDir/");
+            $fileCreator->generateFunctionsList($res->methods, "$genDir/functionsList.php");
+            $fileCreator->generateDeprecatedList($missingMethods, "$genDir/deprecated.php");
+            $fileCreator->generateRectorFile($res->methods, "$genDir/rector-migrate.php");
         }
 
-        foreach ($modules as $moduleName => $foo) {
+        foreach (\array_keys($modules) as $moduleName) {
+            $fileCreator->generateVersionSplitters($moduleName, __DIR__ . "/../../generated/", \array_keys($versions));
             $fileCreator->createExceptionFile((string) $moduleName);
         }
+
+        $fileCreator->generateVersionSplitters("deprecated", __DIR__ . "/../../generated/", \array_keys($versions));
 
         $this->runCsFix($output);
 
         // Finally, let's edit the composer.json file
         $output->writeln('Editing composer.json');
-        ComposerJsonEditor::editComposerFileForGeneration(\array_values($modules));
+        ComposerJsonEditor::editComposerFileForGeneration(\array_keys($modules));
 
         return 0;
     }
@@ -71,7 +92,7 @@ class GenerateCommand extends Command
             \unlink($exception);
         }
 
-        $files = \glob(__DIR__.'/../../generated/*.php');
+        $files = \glob(__DIR__.'/../../generated/*/*.php');
         if ($files === false) {
             throw new \RuntimeException('Failed to require the generated files');
         }

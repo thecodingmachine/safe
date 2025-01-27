f99fa0371ee46e97274f0d75c514a148c699a419
Shish <shish@shishnet.org>
2025-01-27 18:43:22 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which were generated in `8.1/apache.php` but are no longer needed and thus not-generated in `8.2/apache.php` will end up in `8.2/deprecated.php`. Perhaps it'd be better to put them at the tail of `8.2/apache.php`? PRs welcome)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    This isn't even remotely tested, just a proof of concept to show what this kind of approach might look like

---
+++ tests/DeprecationTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class DeprecationTest extends TestCase
+{
+    public function testFunctionWhichBecameSafe(): void
+    {
+        // In 8.1, array_flip is non-safe, so we generate a safe wrapper.
+        // In 8.2, array_flip is safe, so we don't need to generate a
+        // safe wrapper -- but if we simply delete the `Safe` function, then
+        // that will break backwards-compatibility. So we need to generate
+        // a no-op wrapper for newer versions.
+        // Either way, the function should _exist_ in all versions from
+        // when it was introduced onwards.
+        $this->assertTrue(
+            function_exists('Safe\array_flip'),
+            "Safe\array_flip should exist, even in php 8.4 (where it ".
+            "is safe natively), because it was unsafe in 8.1"
+        );
+        $this->assertEquals(
+            \Safe\array_flip([1 => 'a', 2 => 'b']),
+            ['a' => 1, 'b' => 2],
+            "Safe\array_flip should work as expected, whether safe-wrapper or no-op-wrapper"
+        )
+    }
+
+    public function testIntroducedFunction(): void
+    {
+        // This function was introduced in php 8.2, so we should only
+        // generate wrappers for it in 8.2 and later
+        if (strpos(PHP_VERSION, "8.1.") === 0) {
+            $this->assertFalse(
+                function_exists('Safe\openssl_cipher_key_length'),
+                "openssl_cipher_key_length was introduced in php 8.2, ".
+                "so it should not exist in php 8.1"
+            );
+        } else {
+            $this->assertTrue(
+                function_exists('Safe\openssl_cipher_key_length'),
+                "openssl_cipher_key_length was introduced in php 8.2, ".
+                "so it should exist in php 8.2 and later"
+            );
+        }
+    }
+}

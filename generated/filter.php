457bea0a3578d9d5a8bb2a44d96d4997af6b34bd
Shish <shish@shishnet.org>
2025-01-28 05:16:26 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which needed safe wrappers in 8.1 but no longer need safe wrappers in 8.2 will have no-op stubs generated instead)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    This isn't even remotely tested, just a proof of concept to show what this kind of approach might look like

--- generated/filter.php
+++ generated/filter.php
@@ -1,48 +1,17 @@
 <?php
 
-namespace Safe;
-
-use Safe\Exceptions\FilterException;
-
-/**
- * This function is useful for retrieving many values without
- * repetitively calling filter_input.
- *
- * @param int $type
- * @param int|array $options
- * @param bool $add_empty
- * @return array|null
- * @throws FilterException
- *
- */
-function filter_input_array(int $type, $options = FILTER_DEFAULT, bool $add_empty = true): ?array
-{
-    error_clear_last();
-    $safeResult = \filter_input_array($type, $options, $add_empty);
-    if ($safeResult === false) {
-        throw FilterException::createFromPhpError();
-    }
-    return $safeResult;
+if (strpos(PHP_VERSION, "8.1.") === 0) {
+    require_once __DIR__ . '/8.1/filter.php';
 }
-
-
-/**
- *
- *
- * @param array $array
- * @param mixed $options
- * @param bool $add_empty Add missing keys as NULL to the return value.
- * @return array|null An array containing the values of the requested variables on success. An array value will be FALSE if the filter fails, or NULL if
- * the variable is not set.
- * @throws FilterException
- *
- */
-function filter_var_array(array $array, $options = FILTER_DEFAULT, bool $add_empty = true): ?array
-{
-    error_clear_last();
-    $safeResult = \filter_var_array($array, $options, $add_empty);
-    if ($safeResult === false) {
-        throw FilterException::createFromPhpError();
-    }
-    return $safeResult;
+if (strpos(PHP_VERSION, "8.2.") === 0) {
+    require_once __DIR__ . '/8.2/filter.php';
+}
+if (strpos(PHP_VERSION, "8.3.") === 0) {
+    require_once __DIR__ . '/8.3/filter.php';
+}
+if (strpos(PHP_VERSION, "8.4.") === 0) {
+    require_once __DIR__ . '/8.4/filter.php';
+}
+if (strpos(PHP_VERSION, "8.5.") === 0) {
+    require_once __DIR__ . '/8.5/filter.php';
 }

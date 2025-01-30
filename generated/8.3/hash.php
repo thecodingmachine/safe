73f30c399dbd952cee8d40c1aceb80e99169329e
Shish <shish@shishnet.org>
2025-01-30 20:06:42 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which needed safe wrappers in 8.1 but no longer need safe wrappers in 8.2 will have no-op stubs generated instead)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    -

---
+++ generated/8.3/hash.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Safe;
+
+use Safe\Exceptions\HashException;
+
+/**
+ *
+ *
+ * @param \HashContext $context Hashing context returned by hash_init.
+ * @param string $filename URL describing location of file to be hashed; Supports fopen wrappers.
+ * @param \HashContext|null $stream_context Stream context as returned by stream_context_create.
+ * @throws HashException
+ *
+ */
+function hash_update_file(\HashContext $context, string $filename, ?\HashContext $stream_context = null): void
+{
+    error_clear_last();
+    if ($stream_context !== null) {
+        $safeResult = \hash_update_file($context, $filename, $stream_context);
+    } else {
+        $safeResult = \hash_update_file($context, $filename);
+    }
+    if ($safeResult === false) {
+        throw HashException::createFromPhpError();
+    }
+}
+
+function hash_hkdf()
+{
+    return \hash_hkdf(...func_get_args());
+}

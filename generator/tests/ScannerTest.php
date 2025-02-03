4cff2ca93ac84b3a75938c232e6f207dccc1ad41
Shish <shish@shishnet.org>
2025-02-03 07:24:09 +0000

    [generator] reduce hard-coding of directory paths
    I want to be able to move some files around, and having specific paths hard-coded all over the place makes it hard to move anything without breaking everything

--- generator/tests/ScannerTest.php
+++ generator/tests/ScannerTest.php
@@ -11,19 +11,19 @@ class ScannerTest extends TestCase
 
     public function testGetMethodsPaths(): void
     {
-        $scanner = new Scanner(__DIR__ . '/../doc/doc-en/en/reference/');
+        $scanner = new Scanner(DocPage::findReferenceDir());
         $paths = $scanner->getFunctionsPaths();
 
-        $this->assertArrayHasKey(__DIR__.'/../doc/doc-en/en/reference/filesystem/functions/chmod.xml', $paths);
-        $this->assertArrayNotHasKey(__DIR__.'/../doc/doc-en/en/reference/spl/appenditerator/getarrayiterator.xml', $paths);
+        $this->assertArrayHasKey(DocPage::findReferenceDir() . '/filesystem/functions/chmod.xml', $paths);
+        $this->assertArrayNotHasKey(DocPage::findReferenceDir() . '/spl/appenditerator/getarrayiterator.xml', $paths);
     }
 
     public function testGetFunctionsPaths(): void
     {
-        $scanner = new Scanner(__DIR__ . '/../doc/doc-en/en/reference/');
+        $scanner = new Scanner(DocPage::findReferenceDir());
         $paths = $scanner->getMethodsPaths();
 
-        $this->assertArrayNotHasKey(__DIR__.'/../doc/doc-en/en/reference/filesystem/functions/chmod.xml', $paths);
-        $this->assertArrayHasKey(__DIR__.'/../doc/doc-en/en/reference/spl/appenditerator/getarrayiterator.xml', $paths);
+        $this->assertArrayNotHasKey(DocPage::findReferenceDir() . '/filesystem/functions/chmod.xml', $paths);
+        $this->assertArrayHasKey(DocPage::findReferenceDir() . '/spl/appenditerator/getarrayiterator.xml', $paths);
     }
 }

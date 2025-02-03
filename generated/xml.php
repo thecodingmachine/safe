c35e3654d7758a32ccd605b204368756940dd3b6
Shish <shish@shishnet.org>
2025-02-03 09:35:24 +0000

    Generate files for multiple versions
    tl;dr:
    
    replace
    
    ```php
    $function_list = read_docs();
    generate_files($function_list, "../generated/");
    ```
    
    with
    
    ```php
    foreach(["8.1", "8.2", "8.3", "8.4"] as $version) {
        exec("cd docs && git checkout $version");
        $function_list = read_docs();
        generate_files($function_list, "../generated/$version/");
    }
    ```
    
    generate a bunch of stubs like generated/misc.php:
    
    ```php
    <?php
    if(PHP_VERSION == "8.1") require_once __DIR__ . "/8.1/misc.php";
    if(PHP_VERSION == "8.2") require_once __DIR__ . "/8.2/misc.php";
    if(PHP_VERSION == "8.3") require_once __DIR__ . "/8.3/misc.php";
    if(PHP_VERSION == "8.4") require_once __DIR__ . "/8.4/misc.php";
    ```
    
    This also automates generation of "deprecated" `safe` functions (ie, instead of hard-coding `deprecated/apache.php`, any functions which needed safe wrappers in 8.1 but no longer need safe wrappers in 8.2 will have no-op stubs generated instead)
    
    Currently the `Exceptions` are shared between all versions, which I _think_ is a good idea? (Thoughts, anybody?)
    
    -

--- generated/xml.php
+++ generated/xml.php
@@ -1,100 +1,17 @@
 <?php
 
-namespace Safe;
-
-use Safe\Exceptions\XmlException;
-
-/**
- * Frees the given XML parser.
- *
- * @param \XMLParser $parser
- * @throws XmlException
- *
- */
-function xml_parser_free(\XMLParser $parser): void
-{
-    error_clear_last();
-    $safeResult = \xml_parser_free($parser);
-    if ($safeResult === false) {
-        throw XmlException::createFromPhpError();
-    }
+if (strpos(PHP_VERSION, "8.1.") === 0) {
+    require_once __DIR__ . '/8.1/xml.php';
 }
-
-
-/**
- * Sets an option in an XML parser.
- *
- * @param \XMLParser $parser A reference to the XML parser to set an option in.
- * @param int $option Which option to set.  See below.
- *
- * The following options are available:
- *
- * XML parser options
- *
- *
- *
- * Option constant
- * Data type
- * Description
- *
- *
- *
- *
- * XML_OPTION_CASE_FOLDING
- * bool
- *
- * Controls whether case-folding is enabled for this
- * XML parser.  Enabled by default.
- *
- *
- *
- * XML_OPTION_PARSE_HUGE
- * bool
- *
- * Allows parsing documents larger than 10 MB.
- * This option should only be enabled when the document size is
- * bounded because this could otherwise lead to a DoS.
- * This option is only available when using libxml2.
- *
- *
- *
- * XML_OPTION_SKIP_TAGSTART
- * integer
- *
- * Specify how many characters should be skipped in the beginning of a
- * tag name.
- *
- *
- *
- * XML_OPTION_SKIP_WHITE
- * bool
- *
- * Whether to skip values consisting of whitespace characters.
- *
- *
- *
- * XML_OPTION_TARGET_ENCODING
- * string
- *
- * Sets which target encoding to
- * use in this XML parser.By default, it is set to the same as the
- * source encoding used by xml_parser_create.
- * Supported target encodings are ISO-8859-1,
- * US-ASCII and UTF-8.
- *
- *
- *
- *
- *
- * @param mixed $value The option's new value.
- * @throws XmlException
- *
- */
-function xml_parser_set_option(\XMLParser $parser, int $option, $value): void
-{
-    error_clear_last();
-    $safeResult = \xml_parser_set_option($parser, $option, $value);
-    if ($safeResult === false) {
-        throw XmlException::createFromPhpError();
-    }
+if (strpos(PHP_VERSION, "8.2.") === 0) {
+    require_once __DIR__ . '/8.2/xml.php';
+}
+if (strpos(PHP_VERSION, "8.3.") === 0) {
+    require_once __DIR__ . '/8.3/xml.php';
+}
+if (strpos(PHP_VERSION, "8.4.") === 0) {
+    require_once __DIR__ . '/8.4/xml.php';
+}
+if (strpos(PHP_VERSION, "8.5.") === 0) {
+    require_once __DIR__ . '/8.5/xml.php';
 }

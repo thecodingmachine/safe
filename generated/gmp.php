d336959bcd1311d8f0253588f140837f29b23424
Shish <shish@shishnet.org>
2025-02-06 22:33:50 +0000

    strpos -> str_contains in generated files

--- generated/gmp.php
+++ generated/gmp.php
@@ -1,5 +1,5 @@
 <?php
 
-if (strpos(PHP_VERSION, "8.1.") === 0) {
+if (str_starts_with(PHP_VERSION, "8.1.")) {
     require_once __DIR__ . '/8.1/gmp.php';
 }
--- generated/sockets.php
+++ generated/sockets.php
@@ -1,17 +1,17 @@
 <?php
 
-if (strpos(PHP_VERSION, "8.1.") === 0) {
+if (str_starts_with(PHP_VERSION, "8.1.")) {
     require_once __DIR__ . '/8.1/sockets.php';
 }
-if (strpos(PHP_VERSION, "8.2.") === 0) {
+if (str_starts_with(PHP_VERSION, "8.2.")) {
     require_once __DIR__ . '/8.2/sockets.php';
 }
-if (strpos(PHP_VERSION, "8.3.") === 0) {
+if (str_starts_with(PHP_VERSION, "8.3.")) {
     require_once __DIR__ . '/8.3/sockets.php';
 }
-if (strpos(PHP_VERSION, "8.4.") === 0) {
+if (str_starts_with(PHP_VERSION, "8.4.")) {
     require_once __DIR__ . '/8.4/sockets.php';
 }
-if (strpos(PHP_VERSION, "8.5.") === 0) {
+if (str_starts_with(PHP_VERSION, "8.5.")) {
     require_once __DIR__ . '/8.5/sockets.php';
 }

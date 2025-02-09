c9bd1ffc50c7610fb84b337ac1a05ca2d1ab0d9b
Shish <shish@shishnet.org>
2025-02-09 14:44:34 +0000

    more generator unit tests

--- generator/tests/XmlDocParser/ScannerTest.php
+++ generator/tests/XmlDocParser/ScannerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Safe\XmlDocParser;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ScannerTest extends TestCase
 {
@@ -25,5 +26,24 @@ class ScannerTest extends TestCase
 
         $this->assertArrayNotHasKey(DocPage::findReferenceDir() . '/filesystem/functions/chmod.xml', $paths);
         $this->assertArrayHasKey(DocPage::findReferenceDir() . '/spl/appenditerator/getarrayiterator.xml', $paths);
+    }
+
+    public function testGetMethods(): void
+    {
+        $scanner = new Scanner(DocPage::findReferenceDir());
+        $functions = $scanner->getFunctionsPaths();
+        $testFunctions = [];
+        foreach ($functions as $name => $info) {
+            // check various branches:
+            // - array = regular module
+            // - bbcode = ignored module
+            // - image = module with overloaded functions
+            if (str_contains($name, "array") || str_contains($name, "bbcode") || str_contains($name, "image")) {
+                $testFunctions[$name] = $info;
+            }
+        }
+        $output = $this->createMock(OutputInterface::class);
+        $response = $scanner->getMethods($testFunctions, $output);
+        $this->assertNotEmpty($response->methods);
     }
 }

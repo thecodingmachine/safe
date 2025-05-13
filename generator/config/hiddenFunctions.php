51a45c524c0f074493565a9deaa39025f19b2b24
Cédric Anne <cedric.anne@gmail.com>
2025-05-13 22:06:28 +0200

    Fix `hash_hkdf()` on PHP 8.1 (#678)

--- generator/config/hiddenFunctions.php
+++ generator/config/hiddenFunctions.php
@@ -16,6 +16,7 @@ return [
     'date', // this function throws an error instead of returning false PHP 8.0, but the doc has only been updated since PHP 8.4
     'getallheaders', // always return an array since PHP 7, see https://github.com/php/doc-en/commit/68e52ef14de33f6752a8fdda1ae83c861c5babdb
     'gmp_random_seed', // this function throws an error instead of returning false since PHP 8.0
+    'hash_hkdf', // this function throws an error instead of returning false since PHP 8.0
     'long2ip', // false return type cannot actually be returned, see https://github.com/php/php-src/pull/13395
     'pack', // this function no longer returns false since PHP 8.0, but the doc has only been updated since PHP 8.4
     'imagesx', // this function throws an error instead of returning false PHP 8.0, see https://github.com/php/doc-en/commit/0462f49fb00dd5abaec3aa322009f2eb40a3279d
